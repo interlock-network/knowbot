1bac80da6181a1b1bd5f0c001da24ce851dfefa3
Blair <blairmunroakusa@wp.computer>
2022-04-07 17:45:50 -0800

    print content from repo file

--- src/kb-bot.py
+++ src/kb-bot.py
@@ -11,6 +11,8 @@ import os
 import discord
 import requests
 from dotenv import load_dotenv
+from github import Github
+from pprint import pprint
 # from discord.ext import commands
 
 load_dotenv()
@@ -19,11 +21,32 @@ SERVER = os.getenv('DISCORD_SERVER')
 
 client = discord.Client()
 
-kb = 'https://api.github.com/users/interlock-network'
+# PAT from blairmunroakusa for dev purposes
+# scope restricted to only access public repo info
+g = Github('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')
 
-def git_api_repolist(arg):
-    kbdata = requests.get(kb).json()
-    return f'{arg} {kbdata}'
+kb = g.get_repo('blairmunroakusa/portfolio')
+
+# interlock = Github().get_user('interlock-network')
+
+class Content:
+    def __init__(self, content):
+        self.content = content
+
+    def __repr__(self):
+        return fr'{self.content}'
+
+def print_content(arg):
+    kbdata = kb.get_contents('README.md')
+    # kbdata = interlock.get_repos()
+    # kbdata = requests.get('https://api.github.com/users/interlock-network').json()
+    # content = Content(kbdata.decoded_content)
+    test = 'testee1\n' + 'testee2\n'
+    test2 = kbdata.decoded_content
+    return f"""{arg} {kbdata.html_url}
+{test2}
+"""
+    # return print(f'testee\n' f'testtwo')
 
 # connect kb-bot
 @client.event
@@ -38,6 +61,6 @@ async def on_ready():
 async def on_message(message):
     """Invoke when a message is received on the server."""
     if (message.content == 'test'):
-        await message.reply(git_api_repolist(message.content))
+        await message.reply(print_content(message.content))
 
 client.run(TOKEN)
